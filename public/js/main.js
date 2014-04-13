84602a9bd2fd91f58772550d203a0b2690907f63
Farhan Syed <nearhan@gmail.com>
2014-04-13 18:36:37 -0400

    added more stuff

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,32 @@
   socket.on('news', function (data) {
     console.log(data);
   });
+
+
+var resultList = $("#resultList")
+  // Submit event handler
+
+  $('#daButton').click(function(e) {
+      var tweetTopic = $(":text").val();
+      console.log(tweetTopic);
+      console.log(e);
+
+      $(":text").val('');
+
+      socket.on(tweetTopic, function (data) {
+          console.log(data);
+          appendToHtml(data);
+      });
+
+      socket.emit('newTopic', { topic: tweetTopic});
+  });
+
+
+
+
+function appendToHtml(data) {
+
+    var row = '<tr> <td>'+ data.tweet + '</td> <td>' + data.sent.score + '</td> </tr>'
+    resultList.append(row);
+
+}
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var fs = require('fs');
 var Twit = require('twit');
+var sentiment = require('sentiment');
+//var topicHash = require('./lib/topicHash')();
+
 
 
 // load settings ==================================
@@ -22,8 +25,28 @@ var T = new Twit(settings.twitter);
 
 
 // Manage socket connections
-var connections = [];
 io.sockets.on('connection', function(socket) {
-    connections.push(socket);
     socket.emit('news', { hello: 'hello!'});
+
+    //setup newTopic listner
+    socket.on('newTopic', function(data) {
+        console.log(data);
+        var dataObj = { track: data.topic };
+        var stream = T.stream('statuses/filter', dataObj);
+
+        //stream setup
+        stream.on('tweet', function(tweetObj) {
+            sentiment(tweetObj.text, function (err, result) {
+                socket.emit(data.topic, { tweet:tweetObj.text, sent: result });
+
+            });
+        });
+
+
+    });
 });
+
+
+
+
+// helpers ====================================
